01d780ad58443264c8066b2fc36200417a555bdb
satyamkr3103 <satyamkr3103@gmail.com>
2025-12-27 13:25:42 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,69 @@
+import os
+from flask import Flask , render_template , request ,redirect # type: ignore
+from flask_sqlalchemy import SQLAlchemy # pyright: ignore[reportMissingImports]
+from datetime import datetime
+
+BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI']="sqlite:///todo.db"
+app.config['SQLALCHEMY_TRACKBACK_MODIFICATION'] = False
+db = SQLAlchemy(app)
+
+class Todo(db.Model):
+    sno = db.Column(db.Integer,primary_key = True)
+    title = db.Column(db.String(200),nullable = False)
+    desc = db.Column(db.String(500),nullable = False)
+    date_created = db.Column(db.DateTime,default = datetime.utcnow)
+
+def __repr__(self) -> str:
+    return f"{self.sno} - {self.title}"
+
+@app.route('/', methods =['GET','POST'])
+def hello_world():
+    if request.method=='POST':
+        title = request.form.get('title')
+        desc = request.form.get('desc')
+
+        if title and desc:
+            todo = Todo(title=title,desc = desc) # type: ignore
+            db.session.add(todo)
+            db.session.commit()
+
+    allTodo = Todo.query.all()
+    return render_template('index.html',allTodo=allTodo)
+    # return 'hello,world!'
+
+@app.route('/show')
+def products():
+    allTodo = Todo.query.all()
+    print(allTodo)
+    return 'this is the product page'
+
+@app.route('/delete<int:sno>')
+def delete(sno):
+    todo = Todo.query.filter_by(sno=sno).first()
+    db.session.delete(todo)
+    db.session.commit()
+    return redirect('/')
+
+@app.route('/update/<int:sno>',methods=['GET','POST'])
+def update(sno):
+    todo = Todo.query.filter_by(sno=sno).first()
+    if request.method == 'POST':
+        title = request.form.get('title')
+        desc = request.form.get('desc')
+
+        if title and desc:
+            todo.title = title # type: ignore
+            todo.desc = desc # type: ignore
+            db.session.add(todo)
+            db.session.commit()
+            return redirect('/')
+    
+    return render_template('update.html',todo=todo)
+
+if __name__=="__main__":
+    with app.app_context():
+        db.create_all()
+    app.run(debug=True)
